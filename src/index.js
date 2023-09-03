c6f9212c90815500407275705c0e5105a7abffb1
GracieHorn <gracieahorn@gmail.com>
2023-09-03 16:41:56 -0400

    create hello world react app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', { className: 'greeting'}, 'Hello, world!');
+ReactDOM.render(element, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <element />
   </React.StrictMode>
 );
 
